abc6107f1669d2aec102fe094b3be4d7bdbc8dd9
et0511 <shinwon0511@gmail.com>
2020-11-11 17:38:41 +0900

    연습문제

--- practices/01.py
+++ practices/01.py
@@ -2,4 +2,14 @@
 # 키보드로 정수 수치를 입력 받아 그것이 3의 배수인지 판단하세요
 
 number = input('수를 입력하세요:')
-print(number, type(number))
+
+try:
+    numbers = int(number)
+except ValueError as e:
+    print('정수가 아닙니다.')
+else:
+    if numbers % 3 == 0:
+        print('3의 배수입니다.')
+    else:
+        print('3의 배수가 아닙니다.')
+
