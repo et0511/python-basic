84feaddd4803dab2eb7ee790ffb5b66b22ce96fb
et0511 <shinwon0511@gmail.com>
2020-11-12 14:31:23 +0900

    range 연습

---
+++ 03/range.py
@@ -0,0 +1,18 @@
+# range
+seq = range(10)
+print(seq, type(seq))
+
+for i in seq:
+    print(i, end=' ')
+else:
+    print('')
+
+for i in range(5, 15):
+    print(i, end=' ')
+else:
+    print('')
+
+for i in range(0,10, 2):
+    print(i, end=' ')
+else:
+    print('')
